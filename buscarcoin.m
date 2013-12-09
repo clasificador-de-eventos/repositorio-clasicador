22382d147b8871d22411cb88b50e90773b07d153
mire-53 <mireia.gonzalez.ferrer@gmail.com>
2013-12-09 11:48:20 +0100

    Update buscarcoin.m

--- buscarcoin.m
+++ buscarcoin.m
@@ -1,3 +1,9 @@
+% This function search matches between tags of image and tags of all classes
+% - imag1: is the image that classified
+% - c1, c2, c3, c4, c5, c6, c7, c8: are the classes that used for classifier imag1
+
+%_________________________________________________________________________________
+
 function solucion=buscarcoin(imag1,c1,c2,c3,c4,c5,c6,c7,c8)
     L=length(imag1);
     L1=length(c1);
