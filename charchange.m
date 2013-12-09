482ba5039fe0d05022153f150109496894354d2a
mire-53 <mireia.gonzalez.ferrer@gmail.com>
2013-12-09 11:29:19 +0100

    Update charchange.m

--- charchange.m
+++ charchange.m
@@ -1,7 +1,7 @@
 %This function is used for the function cellstrnum2csv, change os a string a char for other%
-% - string: is the string that contains the char that change
-% - old_char: is the old char
-% - new_char: is the new char
+% - string: is the string that contains the char that change%
+% - old_char: is the old char%
+% - new_char: is the new char%
 
 %__________________________________________________________________________________________%
 
